eb0d45ecf984f6e2534d5e4c2f92ebbcdf2156bb
anjanapriya.konapala6@gmail.com <anjanapriya.konapala6@gmail.com>
2026-07-07 23:00:34 +0530

    addeed repo methods

--- decision-service/src/main/java/com/decision/decision_service/repository/UserDeviceRegistryRepository.java
+++ decision-service/src/main/java/com/decision/decision_service/repository/UserDeviceRegistryRepository.java
@@ -4,9 +4,14 @@ import com.decision.decision_service.model.entity.UserDeviceRegistry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserDeviceRegistryRepository extends JpaRepository<UserDeviceRegistry, UUID> {
-
+    Boolean existsByUserIdAndDeviceIdAndDeletedAtIsNull(String userId, String deviceId);
+    Optional<UserDeviceRegistry> findByUserIdAndDeviceIdAndDeletedAtIsNull(String userId, String deviceId);
+    List<UserDeviceRegistry> findByUserIdAndDeletedAtIsNull(String userId);
 }
+
