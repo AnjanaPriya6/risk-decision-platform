a766e972df5ff9a8bb78d6949474206df1d6f89d
anjanapriya.konapala6@gmail.com <anjanapriya.konapala6@gmail.com>
2026-07-07 21:58:46 +0530

    added DecisionAudit remaining fields, 3 fields still remain

--- decision-service/src/main/java/com/decision/decision_service/model/entity/DecisionAudit.java
+++ decision-service/src/main/java/com/decision/decision_service/model/entity/DecisionAudit.java
@@ -1,7 +1,9 @@
 package com.decision.decision_service.model.entity;
 
+import com.decision.decision_service.model.enums.ChannelType;
 import com.decision.decision_service.model.enums.DecisionOutcome;
 import com.decision.decision_service.model.enums.EventType;
+import com.decision.decision_service.model.enums.StepUpResult;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -37,5 +39,31 @@ public class DecisionAudit {
     @Column(name = "decision", nullable = false, updatable = false, columnDefinition = "decision_outcome_enum")
     private DecisionOutcome decision;
 
+    //not sure on how to write the arrays :(
+
+    @Column(name = "risk_score", updatable = false, nullable = false)
+    private double risk_score;
+
+    @Column(name = "policy_version", nullable = false, updatable = false)
+    private String policyVersion;
+
+    @Column(name = "model_version", nullable = false, updatable = false)
+    private String model_version;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "channel", updatable = false, columnDefinition = "channel_enum")
+    private ChannelType channel;
+
+    @Column(name = "decision_time_ms", updatable = false)
+    private double decisionTimeMs;
+
+    @Column(name = "ip_address", updatable = false)
+    private String ipAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "step_up_outcome", updatable = false, columnDefinition = "step_up_outcome_enum")
+    private StepUpResult stepUpOutcome;
+
+    //don't know about the rest two columns with timestamps
 
 }
